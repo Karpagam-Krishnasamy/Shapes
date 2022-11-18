578ee84ff44ab06a3c0c001d3c806398bb49954a
Karpagam-Krishnasamy <karpagamkrishnasamy@gmail.com>
2022-11-18 11:55:44 +0530

    Refactored.

--- src/services/MovementManager.js
+++ src/services/MovementManager.js
@@ -2,64 +2,75 @@ import { find } from '@laufire/utils/collection';
 
 const half = 0.5;
 
-const DirectionProps = {
-	left: { x: -1, y: 0, direction: 'right', border: 0, axis: 'x' },
-	right: { x: 1, y: 0, direction: 'left', border: 100, axis: 'x' },
-	top: { x: 0, y: -1, direction: 'bottom', border: 0, axis: 'y' },
-	bottom: { x: 0, y: 1, direction: 'top', border: 100, axis: 'y' },
+const Directions = {
+	left: { x: -1, y: 0, opposite: 'right', border: 0, axis: 'x' },
+	right: { x: 1, y: 0, opposite: 'left', border: 100, axis: 'x' },
+	top: { x: 0, y: -1, opposite: 'bottom', border: 0, axis: 'y' },
+	bottom: { x: 0, y: 1, opposite: 'top', border: 100, axis: 'y' },
 };
 
-const getCollidePosition = (
-	shape, distance, backgroundSize
-) => {
+const getCollidingPosition = (shape, { distance, backgroundSize }) => {
 	const newPosition = moveNext(shape, distance);
+	const { size, direction } = shape;
+	const { x, y } = Directions[direction];
 
-	newPosition.x += DirectionProps[shape.direction].x * (shape.size * half);
-	newPosition.y += DirectionProps[shape.direction].y * (shape.size * half);
+	newPosition.x += x * (size * half);
+	newPosition.y += y * (size * half);
 
-	return find(newPosition, (value) => value < 0 || value > backgroundSize);
+	return find(newPosition, (value) =>
+		value < 0 || value > backgroundSize);
 };
 
 const getBounceDistance = (
-	shape, collidedPosition, backgroundSize, distance
-) =>
-	shape[DirectionProps[shape.direction].axis]
-	- ((DirectionProps[shape.direction].x || DirectionProps[shape.direction].y)
-	* (DirectionProps[shape.direction].border
-		- (collidedPosition % backgroundSize)
-		- (shape.size * half)));
+	shape, collidingPosition
+	, { backgroundSize }
+) => {
+	const { direction, size } = shape;
+	const { axis, x, y, border } = Directions[direction];
+	const overflowValue = collidingPosition % backgroundSize;
+
+	return shape[axis] - ((x || y)
+		* (border - overflowValue - size * half));
+};
 
 const bounceBack = (
-	shape, collidedPosition, backgroundSize, distance
-) => ({
-	...moveNext({
-		...shape,
-		direction: DirectionProps[shape.direction].direction,
-		life: shape.life--,
-	}
-	, getBounceDistance(
-		shape, collidedPosition, backgroundSize, distance
-	)),
-});
+	shape, collidingPosition, config
+) => {
+	const { opposite } = Directions[shape.direction];
 
-const moveNext = (shape, distance) => ({
-	...shape,
-	x: shape.x + (DirectionProps[shape.direction].x * distance),
-	y: shape.y + (DirectionProps[shape.direction].y * distance),
-});
+	return {
+		...moveNext({
+			...shape,
+			direction: opposite,
+			life: shape.life--,
+		},
+		getBounceDistance(
+			shape, collidingPosition, config
+		)),
+	};
+};
+
+const moveNext = (shape, distance) => {
+	const { x, y } = Directions[shape.direction];
+
+	return {
+		...shape,
+		x: shape.x + (x * distance),
+		y: shape.y + (y * distance),
+	};
+};
 
 const MovementManager = {
-	move: ({ state: { shapes = [] }, config: { distance, backgroundSize }}) =>
+	move: ({ state: { shapes = [] }, config }) =>
 		shapes.map((shape) => {
-			const collidedPosition = getCollidePosition(
-				shape, distance, backgroundSize
-			);
+			const collidingPosition = getCollidingPosition(shape, config);
+			const { distance } = config;
 
 			return {
 				...shape,
-				...collidedPosition
+				...collidingPosition
 					? bounceBack(
-						shape, collidedPosition, backgroundSize, distance
+						shape, collidingPosition, config
 					)
 					: moveNext(shape, distance),
 			};
