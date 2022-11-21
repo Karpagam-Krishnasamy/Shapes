a8e6ed688b6757312f393184ce02c8eecc3bdb29
Karpagam-Krishnasamy <karpagamkrishnasamy@gmail.com>
2022-11-21 16:46:51 +0530

    Added color change concept durin click for triangle.

--- src/components/Triangle.js
+++ src/components/Triangle.js
@@ -2,23 +2,25 @@ import React from 'react';
 
 const half = 0.5;
 
-const Triangle = ({
-	data: { size, x, y, life },
-	config: { backgroundColors },
-}) =>
-	<div { ...{
-		className: 'centre',
-		style: {
-			width: 0,
-			height: 0,
-			left: `${ x }vmin`,
-			top: `${ y }vmin`,
-			borderLeft: `${ size * half }vmin solid transparent`,
-			borderRight: `${ size * half }vmin solid transparent`,
-			borderBottom: `${ size }vmin solid ${ backgroundColors[life] }`,
-		},
-	}
-	}
-	/>;
+const Triangle = ({	data,	actions }) => {
+	const { size, x, y, colorCode } = data;
+
+	return (
+		<div
+			{ ...{
+				className: 'centre',
+				style: {
+					width: 0,
+					height: 0,
+					left: `${ x }vmin`,
+					top: `${ y }vmin`,
+					borderLeft: `${ size * half }vmin solid transparent`,
+					borderRight: `${ size * half }vmin solid transparent`,
+					borderBottom: `${ size }vmin solid hsl(${ colorCode } 100% 50%)`,
+				},
+			} }
+			onClick={ () => actions.changeColor(data) }
+		/>);
+};
 
 export default Triangle;
